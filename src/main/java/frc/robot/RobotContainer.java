dd2637910001f864ff27300dce9d8d7af0b83d65
Paul Brockmeyer <brockzilla@gmail.com>
2026-03-07 21:11:20 -0800

    Remember to move the hood down when we're done shooting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -76,7 +76,7 @@ public class RobotContainer {
         NamedCommands.registerCommand("rotateToHub", Commands.defer(() -> poseManager.rotateToHubCenterCommand(), Set.of(drivetrain)));
         NamedCommands.registerCommand("enterSlowMode", Commands.defer(() -> Commands.runOnce(() -> slowMode = !slowMode), Set.of(drivetrain)));
         // NamedCommands.registerCommand("consolidatePose", Commands.defer(() -> Commands.runOnce(() -> drivetrain.getLocalizer().resetPose(new Pose2d(0,0, Rotation2d.fromRotations(0.0)))), Set.of(drivetrain)));
-        NamedCommands.registerCommand("rangeToHub", Commands.defer(() -> poseManager.rangeToPointCommand(Constants.FieldConstants.getTargetHubCenter(), 2.0), Set.of(drivetrain)));
+        //NamedCommands.registerCommand("rangeToHub", Commands.defer(() -> poseManager.rangeToPointCommand(Constants.FieldConstants.getTargetHubCenter(), 2.0), Set.of(drivetrain)));
         NamedCommands.registerCommand("spinUp", Commands.defer(() -> shooter.setStateCommand(Shooter.State.SPINNING_UP), Set.of(shooter)));
         NamedCommands.registerCommand("shoot", Commands.defer(() -> shootCommand().withTimeout(6.0), Set.of(shooter, hopper, intake)));
         NamedCommands.registerCommand("stopShoot", Commands.defer(() -> shooter.setStateCommand(Shooter.State.INACTIVE), Set.of(shooter)));
@@ -296,7 +296,9 @@ public class RobotContainer {
         return Commands.parallel(
                 shootSequence,
                 poseManager.trackTargetCommand(driverX, driverY),
-                shooter.positionHoodForTargetCommand(targetDistance));
+                shooter.positionHoodForTargetCommand(targetDistance))
+                .finallyDo((interrupted) -> CommandScheduler.getInstance().schedule(
+                        shooter.hoodToRotationsCommand(Constants.ShooterConstants.POSITION_HOOD_MIN)));
     }
 
     
